927cdf17940a829a1956c1aceece9fe3842544eb
Mykeze <99393785+Mykeze@users.noreply.github.com>
2022-02-11 15:40:42 +0100

    write in function

--- exo8.php
+++ exo8.php
@@ -2,12 +2,18 @@
 
 $nbr = 215218;
 
-$parité = $nbr % 2;
+echo EstPair($nbr);
 
-if ($parité == 0) {
-    echo "Le nombre " . $nbr . " est pair";
-} else {
-    echo "Le nombre " . $nbr . " n'est pas pair";
+function EstPair($nombre)
+{
+
+    $parité = $nombre % 2;
+
+    if ($parité == 0) {
+        return "Le nombre " . $nombre . " est pair";
+    } else {
+        return "Le nombre " . $nombre . " n'est pas pair";
+    }
 }
 
 ?>

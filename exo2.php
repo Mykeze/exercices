31c47ce72ddc0106d3fbec3fdcae87fef69d91c8
Mykeze <99393785+Mykeze@users.noreply.github.com>
2022-02-11 15:09:04 +0100

    rename exo2.php

---
+++ exo2.php
@@ -0,0 +1,12 @@
+<?php
+
+$a=2;
+$b=5;
+$Hypothenuse=0;
+$resultat=0;
+
+    $Hypothenuse = round(sqrt($a**2 + $b**2),2);
+
+    $resultat = "L'hypothenuse est : " . $Hypothenuse . "<br>";
+    echo $resultat ;
+?>

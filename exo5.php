ba1b5d1b00e63b2a24b960b8b3bfda39a9167df6
Mykeze <99393785+Mykeze@users.noreply.github.com>
2022-02-11 15:06:31 +0100

    re-init

--- exo5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$init=26;
-
-for ($i = 1; $i < 26; $i++) {
-    $init-=1;
-    echo $init . '<br>';
-}
-?>
--- exo7.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$n=3;
-
-$serie = $n*($n+1)/2;
-
-echo "la somme des premiers entiers de $n est $serie";
-
-?>
